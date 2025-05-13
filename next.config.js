3f804392d585e796b77e52dd4e6b6489e6e1edd1
Nishi-Taiga <122674422+Nishi-Taiga@users.noreply.github.com>
2025-05-13 10:26:01 +0900

    Update next.config.js to properly support App Router

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  transpilePackages: ["@radix-ui/react-toast"],
 }
 
 module.exports = nextConfig
-  
